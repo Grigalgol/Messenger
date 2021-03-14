1cff0008c0ed943eafd03721c6f02512f6f737b0
Grigoriy <grisha101202@yandex,ru>
2021-03-14 20:10:57 +0300

    create intent

--- app/src/main/java/com/example/messenger/MainActivity.java
+++ app/src/main/java/com/example/messenger/MainActivity.java
@@ -2,18 +2,25 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextSendMsg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editTextSendMsg = findViewById(R.id.editTextMessage);
     }
 
     public void OnClickSendMessage(View view) {
-        
+        String msg = editTextSendMsg.getText().toString();
+        Intent intent = new Intent(this, ReseveMessageActivity.class);
+        intent.putExtra("msg",msg);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/messenger/ReseveMessageActivity.java
+++ app/src/main/java/com/example/messenger/ReseveMessageActivity.java
@@ -2,13 +2,20 @@ package com.example.messenger;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ReseveMessageActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reseve_message);
+        Intent intent = getIntent();
+        String msg = intent.getStringExtra("msg");
+        textView = findViewById(R.id.textView);
+        textView.setText(msg);
     }
 }
