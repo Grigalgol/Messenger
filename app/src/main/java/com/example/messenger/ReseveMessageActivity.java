a1245ecd32ec42e001e5ccfaf29f83774495aecf
Grigoriy <grisha101202@yandex,ru>
2021-03-14 19:49:17 +0300

    make maket and create new Activity

--- app/src/main/java/com/example/messenger/ReseveMessageActivity.java
+++ app/src/main/java/com/example/messenger/ReseveMessageActivity.java
@@ -3,17 +3,12 @@ package com.example.messenger;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class ReseveMessageActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-
-    public void OnClickSendMessage(View view) {
-        
+        setContentView(R.layout.activity_reseve_message);
     }
 }
